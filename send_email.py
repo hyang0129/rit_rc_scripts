d3e9eac301cbc7c3df50988a1b63a0708d38cb25
Hong <hooong.yang@gmail.com>
2023-03-28 14:01:19 -0400

    test

--- send_email.py
+++ send_email.py
@@ -2,28 +2,58 @@ import smtplib
 from email.message import EmailMessage
 
 import time
+import glob
 
 
 sent = False
 while not sent:
 
+    files = glob.glob('*.err')
+
+    with open(files[0], 'r') as errlog:
+
+        lines = errlog.read()
+
+        for line in lines:
+            if 'rc.rit' in line:
+
+                splits = line.split('/')
+
+                for split in splits:
+                    if 'rc.rit' in split:
+                        break
+
+                port = split.split(':')[-1]
+                host = split.split('.')[0]
 
 
+                base_command = f'ssh -i .ssh/rit_rc_key hy3134@sporcsubmit.rc.rit.edu -L {port}:{host}:{port}'
 
-    # set your email and password
-    # please use App Password
-    email_address = "mail.manager.rit@gmail.com"
-    email_password = "gsnflhangzqckikh"
 
-    # create email
-    msg = EmailMessage()
-    msg['Subject'] = "Email subject"
-    msg['From'] = email_address
-    msg['To'] = "hooong.yang@gmail.com"
-    msg.set_content("This is eamil message")
+                url = line.split(' ')[-1].replace('{host}.rc.rit.edu', '127.0.0.1')
 
-    # send email
-    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-        smtp.login(email_address, email_password)
-        smtp.send_message(msg)
 
+                # set your email and password
+                # please use App Password
+                email_address = "mail.manager.rit@gmail.com"
+                email_password = "gsnflhangzqckikh"
+
+                # create email
+                msg = EmailMessage()
+                msg['Subject'] = "Email subject"
+                msg['From'] = email_address
+                msg['To'] = "hooong.yang@gmail.com"
+                msg.set_content(f"""
+                {base_command}
+                {url}              
+                """)
+
+                # send email
+                with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+                    smtp.login(email_address, email_password)
+                    smtp.send_message(msg)
+
+
+                sent = True
+
+                break
