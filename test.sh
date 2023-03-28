b68f4672697b1d09260003dbf955088648c45b6b
Hong <hooong.yang@gmail.com>
2023-03-28 14:24:48 -0400

    test

--- test.sh
+++ test.sh
@@ -17,7 +17,8 @@
 #SBATCH --ntasks=1			# How many tasks per node
 #SBATCH --cpus-per-task=4		# Number of CPUs per task
 #SBATCH --mem-per-cpu=10g		# Memory per CPU
-#SBATCH --gres=gpu:p4:1
+#SBATCH --gres=gpu:a100:1
+
 
 
 echo "Setting Up Jupyter Server"
