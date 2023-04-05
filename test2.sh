6ad84c4cdc59a91ccabf4137f2befc14c8196c4a
Hong <hooong.yang@gmail.com>
2023-04-05 13:00:05 -0400

    test

--- test2.sh
+++ test2.sh
@@ -27,6 +27,12 @@ echo "Loading Packages"
 source ~/conda/etc/profile.d/conda.sh
 conda activate
 
+export XLA_FLAGS=--xla_gpu_cuda_data_dir=/home/hy3134/conda/lib
+# note that XLA flags dir must resemble /lib/nvvm/libdevice/libdevice.10.bc
+
+CUDNN_PATH=$(dirname $(python -c "import nvidia.cudnn;print(nvidia.cudnn.__file__)"))
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/:$CUDNN_PATH/lib
+
 spack load /igzaycn # cuda 11.8
 spack load /dr4ipev # gcc 12.2
 
