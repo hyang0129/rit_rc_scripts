965870168caa615c42ce9b0fdedf8f4b652cbad5
Hong <hooong.yang@gmail.com>
2023-07-19 12:30:50 -0400

    test

--- test2.sh
+++ test2.sh
@@ -1,24 +1,5 @@
 #!/bin/bash -l
 
-#SBATCH --job-name=testJob		# Name for your job
-#SBATCH --comment="Testing Job"		# Comment for your job
-
-#SBATCH --account=ngafid		# Project account to run your job under
-#SBATCH --partition=tier3		# Partition to run your job on
-
-#SBATCH --output=%x_%j.out		# Output file
-#SBATCH --error=%x_%j.err		# Error file
-
-#SBATCH --mail-user=hy3134@rit.edu	# Email address to notify
-#SBATCH --mail-type=END			# Type of notification emails to send
-
-#SBATCH --time=0-02:00:00		# Time limit
-#SBATCH --nodes=1			# How many nodes to run on
-#SBATCH --ntasks=1			# How many tasks per node
-#SBATCH --cpus-per-task=4		# Number of CPUs per task
-#SBATCH --mem-per-cpu=10g		# Memory per CPU
-#SBATCH --gres=gpu:a100:1
-
 
 echo "Setting Up Jupyter Server"
 
@@ -29,6 +10,8 @@ conda activate
 
 export XLA_FLAGS=--xla_gpu_cuda_data_dir=/home/hy3134/conda/lib
 # note that XLA flags dir must resemble /lib/nvvm/libdevice/libdevice.10.bc
+# this needs to be set to your own home drive that contains the libdevice.10.bc, but only relevant for tensorflow
+# you should be able to skip this XLA flag completely if we only doing pytorch
 
 CUDNN_PATH=$(dirname $(python -c "import nvidia.cudnn;print(nvidia.cudnn.__file__)"))
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/:$CUDNN_PATH/lib
@@ -38,4 +21,4 @@ spack load /dr4ipev # gcc 12.2
 
 
 echo "Launching Jupyter Server"
-jupyter lab --ip=0.0.0.0 --no-browser
+jupyter lab --ip=0.0.0.0 --no-browser --port=8888
