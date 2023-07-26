ad6340667d03799515456cd13b09bb195c60eb74
Hong <hooong.yang@gmail.com>
2023-07-26 14:54:01 -0400

    test

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=testJob		# Name for your job
 #SBATCH --comment="Testing Job"		# Comment for your job
 
-#SBATCH --account=ngafid		# Project account to run your job under
+#SBATCH --account=icmab		# Project account to run your job under
 #SBATCH --partition=tier3		# Partition to run your job on
 
 #SBATCH --output=%x_%j.out		# Output file
@@ -12,43 +12,29 @@
 #SBATCH --mail-user=hy3134@rit.edu	# Email address to notify
 #SBATCH --mail-type=END			# Type of notification emails to send
 
-#SBATCH --time=0-02:00:00		# Time limit
+#SBATCH --time=0-12:00:00		# Time limit
 #SBATCH --nodes=1			# How many nodes to run on
 #SBATCH --ntasks=1			# How many tasks per node
-#SBATCH --cpus-per-task=4		# Number of CPUs per task
+#SBATCH --cpus-per-task=24		# Number of CPUs per task
 #SBATCH --mem-per-cpu=10g		# Memory per CPU
 #SBATCH --gres=gpu:a100:1
 
 echo "Setting Up Jupyter Server"
 
-hostname				# Run the command hostname
+echo "Loading Packages"
 
-echo "Loading Spack Package"
+source ~/conda/etc/profile.d/conda.sh
+conda activate
 
-#spack load /4oiwjzp # jupyter server w py3.10
-#spack load /igzaycn # cuda 11.8
-#spack load /dr4ipev # gcc 12.2
-#
+export XLA_FLAGS=--xla_gpu_cuda_data_dir=/home/hy3134/conda/lib
+# note that XLA flags dir must resemble /lib/nvvm/libdevice/libdevice.10.bc
 
-spack env activate engl-581-22102601
-spack load
+CUDNN_PATH=$(dirname $(python -c "import nvidia.cudnn;print(nvidia.cudnn.__file__)"))
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/:$CUDNN_PATH/lib
 
-echo "Launcher Jupyter Server"
-jupyter notebook --ip=0.0.0.0 --no-browser &
-
-#echo "Installing Requirements"
-#pip install -r requirements.txt
-
-#echo "Running loop to send email"
-#python send_email.py
-
-#echo "Running GPU Usage Check"
-#python check_gpu_usage.py
-
-#echo 'Exiting Due to Insufficient GPU Usage'
-
-sleep 7200
+spack load /igzaycn # cuda 11.8
+spack load /dr4ipev # gcc 12.2
 
 
-
-
+echo "Launching Jupyter Server"
+jupyter lab --ip=0.0.0.0 --no-browser --port=8889
