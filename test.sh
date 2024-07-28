f2c9fdd4331bfbb0bdd06e7d4f9252ed0397d661
Hong <hooong.yang@gmail.com>
2024-07-28 15:37:16 -0400

    test

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@
 #SBATCH --comment="Testing Job"		# Comment for your job
 
 #SBATCH --account=icmab		# Project account to run your job under
-#SBATCH --partition=tier3		# Partition to run your job on
+#SBATCH --partition=tier2		# Partition to run your job on
 
 #SBATCH --output=%x_%j.out		# Output file
 #SBATCH --error=%x_%j.err		# Error file
@@ -12,12 +12,12 @@
 #SBATCH --mail-user=hy3134@rit.edu	# Email address to notify
 #SBATCH --mail-type=END			# Type of notification emails to send
 
-#SBATCH --time=0-12:00:00		# Time limit
+#SBATCH --time=0-72:00:00		# Time limit
 #SBATCH --nodes=1			# How many nodes to run on
 #SBATCH --ntasks=1			# How many tasks per node
-#SBATCH --cpus-per-task=24		# Number of CPUs per task
+#SBATCH --cpus-per-task=10		# Number of CPUs per task
 #SBATCH --mem-per-cpu=10g		# Memory per CPU
-#SBATCH --gres=gpu:p4:1
+#SBATCH --gres=gpu:v100:1
 
 echo "Setting Up Jupyter Server"
 
