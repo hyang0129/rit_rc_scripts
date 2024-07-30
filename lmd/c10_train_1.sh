41400a575301fe024c2a8ed6c411f71f6b4112ff
Hong <hooong.yang@gmail.com>
2024-07-30 15:31:43 -0400

    test

--- lmd/c10_train_1.sh
+++ lmd/c10_train_1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 
-#SBATCH --job-name=cars_train_1	# Name for your job
+#SBATCH --job-name=c10_train_1.sh# Name for your job
 #SBATCH --comment="Testing Job"		# Comment for your job
 
 #SBATCH --account=icmab		# Project account to run your job under
